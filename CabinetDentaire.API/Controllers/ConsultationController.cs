bc09699bf7c1771237af1f4a78361727e587ede2
zakaria daoudi <zakaria.daoudi@seven-app.net>
2022-05-06 23:05:14 +0000

    remove .vs/

--- CabinetDentaire.API/Controllers/ConsultationController.cs
+++ CabinetDentaire.API/Controllers/ConsultationController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CabinetDentaire.API.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class ConsultationController : ControllerBase
